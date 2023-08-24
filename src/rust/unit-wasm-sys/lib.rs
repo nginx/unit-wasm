5f8623b45b1ff9c96f0db57fcc41eb5e1bbca90e
Andrew Clayton <a.clayton@nginx.com>
2023-08-24 17:39:49 +0100

    Rust: Don't disable dead code warnings
    Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

--- src/rust/unit-wasm-sys/lib.rs
+++ src/rust/unit-wasm-sys/lib.rs
@@ -9,7 +9,6 @@
 mod bindings {
     #![allow(non_upper_case_globals)]
     #![allow(non_camel_case_types)]
-    #![allow(dead_code)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
     include!("macros.rs");
