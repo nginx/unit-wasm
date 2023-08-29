nginx/unit-wasm

commit bf968c99ce109c63fded62ec573c4bc247dc9c0a

Rust/rusty: Add uwr_get_http_content_str()
This function is like uwr_get_http_content() except that it returns a
Rust str.

This is more convenient if you want to operate on the body content
within Rust.

It's worth noting that uwr_get_http_content() returns a non null
terminated buffer which makes it tricky to work with in Rust.

Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

src/rust/unit-wasm-sys/rusty.rs
@@ -9,6 +9,8 @@ use std::ffi::c_char;
 use std::ffi::c_void;
 use std::ffi::CStr;
 use std::ptr::null_mut;
+use std::slice;
+use std::str;
 
 #[macro_export]
 macro_rules! C2S {
@@ -99,6 +101,16 @@ pub fn uwr_get_http_content(ctx: *const luw_ctx_t) -> *const u8 {
 
 pub fn uwr_get_http_content_len(ctx: *const luw_ctx_t) -> usize {
     unsafe { luw_get_http_content_len(ctx) }
+}
+
+pub fn uwr_get_http_content_str(ctx: *const luw_ctx_t) -> &'static str {
+    unsafe {
+        let slice = slice::from_raw_parts(
+            uwr_get_http_content(ctx),
+            uwr_get_http_content_len(ctx),
+        );
+        str::from_utf8(slice).unwrap()
+    }
 }
 
 pub fn uwr_get_http_content_sent(ctx: *const luw_ctx_t) -> usize {

commit 04197e036a01d3826e0529f1396924a868bb4dd2

Rust/rusty: Make use of uwr_get_http_total_content_sent()
When the uwr_get_http_content_str() function, which returns the request
body content as a string, was added it used uwr_get_http_content_len()
to determine the length of the returned string (the request body content
is not null-terminated).

This could potentially in some circumstances return too much data if the
request was split over multiple calls into the Wasm module and
uwr_get_http_content_str() was called before all the data had actually
been received.

Instead use the newly introduced uwr_get_http_total_content_sent()
function to determine the amount of data to return in the string as it
currently stands.

Fixes: bf968c9 ("Rust/rusty: Add uwr_get_http_content_str()")
Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

src/rust/unit-wasm-sys/rusty.rs
@@ -115,7 +115,7 @@ pub fn uwr_get_http_content_str(ctx: *const luw_ctx_t) -> &'static str {
     unsafe {
         let slice = slice::from_raw_parts(
             uwr_get_http_content(ctx),
-            uwr_get_http_content_len(ctx),
+            uwr_get_http_total_content_sent(ctx),
         );
         str::from_utf8(slice).unwrap()
     }