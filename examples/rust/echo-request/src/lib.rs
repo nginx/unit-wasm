nginx/unit-wasm

commit 2cf492f49cfb05c5083c99a38634c2f40b6d3e84

examples/rust: Fix some new rustc warnings
With at least

  rustc 1.79.0 (129f3b996 2024-06-10) (Fedora 1.79.0-3.fc40)

We were getting warnings when building the rust examples like

warning: creating a mutable reference to mutable static is discouraged
  --> src/lib.rs:75:40
   |
75 |     let ctx: *mut luw_ctx_t = unsafe { &mut CTX };
   |                                        ^^^^^^^^ mutable reference to mutable static
   |
   = note: for more information, see issue #114447 <https://github.com/rust-lang/rust/issues/114447>
   = note: this will be a hard error in the 2024 edition
   = note: this mutable reference has lifetime `'static`, but if the static gets accessed (read or written) by any other means, or any other reference is created, then any further use of this mutable reference is Undefined Behavior
   = note: `#[warn(static_mut_refs)]` on by default
help: use `addr_of_mut!` instead to create a raw pointer
   |
75 |     let ctx: *mut luw_ctx_t = unsafe { addr_of_mut!(CTX) };
   |                                        ~~~~~~~~~~~~~~~~~

So do like it says and use the addr_of_mut!() macro.

Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

examples/rust/echo-request/src/lib.rs
@@ -11,7 +11,7 @@ use unit_wasm::rusty::*;
 use std::ffi::CStr;
 use std::os::raw::c_char;
 use std::os::raw::c_void;
-use std::ptr::null_mut;
+use std::ptr::{addr_of_mut, null_mut};
 
 // Buffer of some size to store the copy of the request
 static mut REQUEST_BUF: *mut u8 = null_mut();
@@ -51,7 +51,7 @@ pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
     uwr_init_ctx(ctx, addr, 4096);
 
     // Set where we will copy the request into
-    uwr_set_req_buf(ctx, unsafe { &mut REQUEST_BUF }, LUW_SRB_NONE);
+    uwr_set_req_buf(ctx, unsafe { addr_of_mut!(REQUEST_BUF) }, LUW_SRB_NONE);
 
     // Define the Response Body Text.
 

examples/rust/large-upload/src/lib.rs
@@ -9,7 +9,7 @@
 use unit_wasm::rusty::*;
 
 use std::fs::File;
-use std::ptr::null_mut;
+use std::ptr::{addr_of_mut, null_mut};
 
 static mut CTX: luw_ctx_t = UWR_CTX_INITIALIZER();
 static mut REQUEST_BUF: *mut u8 = null_mut();
@@ -32,14 +32,18 @@ pub unsafe extern "C" fn uwr_response_end_handler() {
 
 #[no_mangle]
 pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
-    let ctx: *mut luw_ctx_t = unsafe { &mut CTX };
+    let ctx: *mut luw_ctx_t = unsafe { addr_of_mut!(CTX) };
     let mut f;
     let bytes_wrote: isize;
     let mut total = unsafe { TOTAL_BYTES_WROTE };
 
     if total == 0 {
         uwr_init_ctx(ctx, addr, 0);
-        uwr_set_req_buf(ctx, unsafe { &mut REQUEST_BUF }, LUW_SRB_NONE);
+        uwr_set_req_buf(
+            ctx,
+            unsafe { addr_of_mut!(REQUEST_BUF) },
+            LUW_SRB_NONE,
+        );
 
         f = File::create("/var/tmp/large-file.dat").unwrap();
     } else {

examples/rust/upload-reflector/src/lib.rs
@@ -8,7 +8,7 @@
 
 use unit_wasm::rusty::*;
 
-use std::ptr::null_mut;
+use std::ptr::{addr_of_mut, null_mut};
 
 static mut CTX: luw_ctx_t = UWR_CTX_INITIALIZER();
 
@@ -72,7 +72,7 @@ pub fn upload_reflector(ctx: *mut luw_ctx_t) -> i32 {
 
 #[no_mangle]
 pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
-    let ctx: *mut luw_ctx_t = unsafe { &mut CTX };
+    let ctx: *mut luw_ctx_t = unsafe { addr_of_mut!(CTX) };
 
     if unsafe { REQUEST_BUF.is_null() } {
         uwr_init_ctx(ctx, addr, 0 /* Response offset */);
@@ -87,7 +87,7 @@ pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
          */
         uwr_set_req_buf(
             ctx,
-            unsafe { &mut REQUEST_BUF },
+            unsafe { addr_of_mut!(REQUEST_BUF) },
             LUW_SRB_APPEND | LUW_SRB_ALLOC | LUW_SRB_FULL_SIZE,
         );
     } else {

commit 89900c002afb77366503330b513c2547b6cf1abd

examples/rust: Remove some redundant unsafe blocks
... as noted by the compiler...

Fixes: 2cf492f ("examples/rust: Fix some new rustc warnings")
Signed-off-by: Andrew Clayton <a.clayton@nginx.com>

examples/rust/echo-request/src/lib.rs
@@ -51,7 +51,7 @@ pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
     uwr_init_ctx(ctx, addr, 4096);
 
     // Set where we will copy the request into
-    uwr_set_req_buf(ctx, unsafe { addr_of_mut!(REQUEST_BUF) }, LUW_SRB_NONE);
+    uwr_set_req_buf(ctx, addr_of_mut!(REQUEST_BUF), LUW_SRB_NONE);
 
     // Define the Response Body Text.
 

examples/rust/large-upload/src/lib.rs
@@ -32,7 +32,7 @@ pub unsafe extern "C" fn uwr_response_end_handler() {
 
 #[no_mangle]
 pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
-    let ctx: *mut luw_ctx_t = unsafe { addr_of_mut!(CTX) };
+    let ctx: *mut luw_ctx_t = addr_of_mut!(CTX);
     let mut f;
     let bytes_wrote: isize;
     let mut total = unsafe { TOTAL_BYTES_WROTE };
@@ -41,7 +41,7 @@ pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
         uwr_init_ctx(ctx, addr, 0);
         uwr_set_req_buf(
             ctx,
-            unsafe { addr_of_mut!(REQUEST_BUF) },
+            addr_of_mut!(REQUEST_BUF),
             LUW_SRB_NONE,
         );
 

examples/rust/upload-reflector/src/lib.rs
@@ -72,7 +72,7 @@ pub fn upload_reflector(ctx: *mut luw_ctx_t) -> i32 {
 
 #[no_mangle]
 pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
-    let ctx: *mut luw_ctx_t = unsafe { addr_of_mut!(CTX) };
+    let ctx: *mut luw_ctx_t = addr_of_mut!(CTX);
 
     if unsafe { REQUEST_BUF.is_null() } {
         uwr_init_ctx(ctx, addr, 0 /* Response offset */);
@@ -87,7 +87,7 @@ pub extern "C" fn uwr_request_handler(addr: *mut u8) -> i32 {
          */
         uwr_set_req_buf(
             ctx,
-            unsafe { addr_of_mut!(REQUEST_BUF) },
+            addr_of_mut!(REQUEST_BUF),
             LUW_SRB_APPEND | LUW_SRB_ALLOC | LUW_SRB_FULL_SIZE,
         );
     } else {